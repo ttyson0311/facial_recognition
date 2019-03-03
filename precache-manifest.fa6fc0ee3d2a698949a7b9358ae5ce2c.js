d25e64b004dc2ae07f75e15e776a30d67d7c427b
ttyson0311 <ttyson0311@gmail.com>
2019-03-03 17:05:26 -0500

    Updates

---
+++ precache-manifest.fa6fc0ee3d2a698949a7b9358ae5ce2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a86be0a41a63c19742",
+    "url": "/github.io/facial_recognition/static/js/runtime~main.22a86be0.js"
+  },
+  {
+    "revision": "8d923f10522a1f87d9f7",
+    "url": "/github.io/facial_recognition/static/js/main.8d923f10.chunk.js"
+  },
+  {
+    "revision": "232436c74d43941b681a",
+    "url": "/github.io/facial_recognition/static/js/1.232436c7.chunk.js"
+  },
+  {
+    "revision": "8d923f10522a1f87d9f7",
+    "url": "/github.io/facial_recognition/static/css/main.dfab63ee.chunk.css"
+  },
+  {
+    "revision": "232436c74d43941b681a",
+    "url": "/github.io/facial_recognition/static/css/1.9afd690b.chunk.css"
+  },
+  {
+    "revision": "59a7642b0abe11fc887e6d4ee43d7505",
+    "url": "/github.io/facial_recognition/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/github.io/facial_recognition/precache-manifest.fa6fc0ee3d2a698949a7b9358ae5ce2c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/github.io/facial_recognition/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
