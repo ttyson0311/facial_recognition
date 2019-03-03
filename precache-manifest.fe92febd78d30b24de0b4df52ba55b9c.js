8078a635dcf0baf6e0876bbec09ae65862d28617
ttyson0311 <ttyson0311@gmail.com>
2019-03-03 17:37:47 -0500

    Updates

---
+++ precache-manifest.fe92febd78d30b24de0b4df52ba55b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a86be0a41a63c19742",
+    "url": "/github.io/facial_recognition/static/js/runtime~main.22a86be0.js"
+  },
+  {
+    "revision": "c386268408c79206c613",
+    "url": "/github.io/facial_recognition/static/js/main.c3862684.chunk.js"
+  },
+  {
+    "revision": "d4825f2351993f16d3df",
+    "url": "/github.io/facial_recognition/static/js/1.d4825f23.chunk.js"
+  },
+  {
+    "revision": "c386268408c79206c613",
+    "url": "/github.io/facial_recognition/static/css/main.1647483e.chunk.css"
+  },
+  {
+    "revision": "d4825f2351993f16d3df",
+    "url": "/github.io/facial_recognition/static/css/1.5cf0029f.chunk.css"
+  },
+  {
+    "revision": "f55bc156836ac1e0468418263ea51518",
+    "url": "/github.io/facial_recognition/index.html"
+  }
+];
