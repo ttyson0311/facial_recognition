93abd1ae478733107e22c8c6dda164ec9f0f03d0
ttyson0311 <ttyson0311@gmail.com>
2019-03-03 18:14:13 -0500

    Updates

---
+++ precache-manifest.7441e6dfe2acc51f7f65a92562589875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c97ecb31a582fb86ab9b",
+    "url": "/facial_recognition/static/js/runtime~main.c97ecb31.js"
+  },
+  {
+    "revision": "a563dbaee6a33be0217e",
+    "url": "/facial_recognition/static/js/main.a563dbae.chunk.js"
+  },
+  {
+    "revision": "e6a86a149b2ed40270e8",
+    "url": "/facial_recognition/static/js/1.e6a86a14.chunk.js"
+  },
+  {
+    "revision": "a563dbaee6a33be0217e",
+    "url": "/facial_recognition/static/css/main.f07b8ac6.chunk.css"
+  },
+  {
+    "revision": "e6a86a149b2ed40270e8",
+    "url": "/facial_recognition/static/css/1.b02b9220.chunk.css"
+  },
+  {
+    "revision": "a9aa32cc3dfa2489835589eb487b8b4c",
+    "url": "/facial_recognition/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.io/facial_recognition/precache-manifest.14372ce2f3b7db7ae99d8c387046259a.js"
+  "/facial_recognition/precache-manifest.7441e6dfe2acc51f7f65a92562589875.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.io/facial_recognition/index.html", {
+workbox.routing.registerNavigationRoute("/facial_recognition/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
