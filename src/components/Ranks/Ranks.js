ff3e6d73f3794901d6042e59371d58b70b9c2e94
ttyson0311 <ttyson0311@gmail.com>
2019-03-03 16:55:28 -0500

    first commit

---
+++ src/components/Ranks/Ranks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ranks = () => {
+  return (
+    <div>
+      <div className="white f3">{"Tanisha, your current rank is .."}</div>
+      <div className="white f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Ranks;
